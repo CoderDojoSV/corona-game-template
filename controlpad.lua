6d6104e000f6bfdcf0e77d6de5576f42f44bea88
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-10-22 16:58:15 -0700

    Fix some controlpad bugs.

--- controlpad.lua
+++ controlpad.lua
@@ -1,9 +1,84 @@
 local ControlPad = {}
 
+local controlPadFunctions = {
+	whenUpPressed = function(controlpad, whenPressed)
+		controlpad.up:removeEventListener("touch")
+		controlpad.up:addEventListener("touch", function(event)
+			if event.phase == "began" then
+				whenPressed()
+			end
+		end)
+	end,
+
+	whenDownPressed = function(controlpad, whenPressed)
+		controlpad.down:removeEventListener("touch")
+		controlpad.down:addEventListener("touch", function(event)
+			if event.phase == "began" then
+				whenPressed()
+			end
+		end)
+	end,
+
+	whenLeftPressed = function(controlpad, whenPressed)
+		controlpad.left:removeEventListener("touch")
+		controlpad.left:addEventListener("touch", function(event)
+			if event.phase == "began" then
+				whenPressed()
+			end
+		end)
+	end,
+
+	whenRightPressed = function(controlpad, whenPressed)
+		controlpad.right:removeEventListener("touch")
+		controlpad.right:addEventListener("touch", function(event)
+			if event.phase == "began" then
+				whenPressed()
+			end
+		end)
+	end,
+
+	show = function(controlpad)
+		controlpad.displayGroup.isVisible = true
+	end,
+
+	hide = function(controlpad)
+		controlpad.displayGroup.isVisible = false
+	end,
+}
+
 ControlPad.newControlPad = function(x, y, radius)
 	local controlpad = {}
 	controlpad.displayGroup = display.newGroup()
-	return controlpad
+
+	controlpad.up = display.newRect(controlpad.displayGroup,
+	radius / 2, 0,
+	radius / 3, radius)
+	controlpad.up:setFillColor(92, 92, 92)
+
+	controlpad.down = display.newRect(controlpad.displayGroup,
+	radius / 2, radius / 3,
+	radius / 3, radius)
+	controlpad.down:setFillColor(92, 92, 92)
+
+	controlpad.left = display.newRect(controlpad.displayGroup,
+	0, radius / 2,
+	radius, radius / 3)
+	controlpad.left:setFillColor(92, 92, 92)
+
+	controlpad.right = display.newRect(controlpad.displayGroup,
+	radius / 3, radius / 2,
+	radius, radius / 3)
+	controlpad.right:setFillColor(92, 92, 92)
+
+	controlpad.show = controlPadFunctions.show
+	controlpad.hide = controlPadFunctions.hide
+	controlpad.whenUpPressed = controlPadFunctions.whenUpPressed
+	controlpad.whenDownPressed = controlPadFunctions.whenDownPressed
+	controlpad.whenRightPressed = controlPadFunctions.whenRightPressed
+	controlpad.whenLeftPressed = controlPadFunctions.whenLeftPressed
+	controlpad.displayGroup.x = x
+	controlpad.displayGroup.y = y
+	return controlpad 
 end
 
 return ControlPad
--- docs/controlpad.md
+++ docs/controlpad.md
@@ -13,20 +13,40 @@ local controlpad = require("controlpad")
 ```
 ## Module functions
 
-* `newControlPad` Create a new ControlPad object
+* `newControlPad()` Create a new ControlPad object
+
+### Creating the control pad
+
+		controlpad.newControlPad(x, y, radius)
+
+#### Parameters
+
+* `x` The pixel display x coordinate of the center of the control pad
+* `y` The pixel display y coordinate of the center of the control pad
+* `radius` The radius in display pixels of the control pad circle
+
+#### Examples
+
+###### Creating a control pad
+
+```lua
+local controlpad = require("controlpad")
+local myControls = controlpad.newControlPad(100, 500, 100)
+```
 
 ## ControlPad object
 
 ### Fields
 
-* `displayGroup`
+* `displayGroup` The display group containing the graphics for the
+control pad
 
 ### Methods
 
-* `whenUpPressed()`
-* `whenDownPressed()`
-* `whenLeftPressed()`
-* `whenRightPressed()`
-* `show()`
-* `hide()`
+* `whenUpPressed()` The function called when up is pressed
+* `whenDownPressed()` The function called when down is pressed
+* `whenLeftPressed()` The function called when left is pressed
+* `whenRightPressed()` The function called when right is pressed
+* `show()` Show the control pad on the screen
+* `hide()` Hide the control pad from the screen
 
