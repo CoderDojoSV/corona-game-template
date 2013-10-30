44d6ebafab8cb3ecfd116baae161bf53281df4a7
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-10-29 17:26:15 -0700

    Add Source Code Pro font and a stars background.

--- main.lua
+++ main.lua
@@ -3,3 +3,32 @@
 -- This is where your code for the game goes! You can use any of the libraries
 -- we created during class by using the `require` function.
 
+
+sourcefonts = require("sourcefonts")
+sourcefonts.printFontNames()
+sourcefonts.findFontByName("source")
+
+display.newText({
+	text = "Source Code Pro Regular",
+	font = sourcefonts.SourceCodePro,
+	fontSize = 48,
+	x = 400,
+	y = 100,
+})
+
+display.newText({
+	text = "Source Code Pro Light",
+	font = sourcefonts.SourceCodePro_Light,
+	fontSize = 48,
+	x = 400,
+	y = 200,
+})
+
+display.newText({
+	text = "Source Code Pro Black",
+	font = sourcefonts.SourceCodePro_Black,
+	fontSize = 48,
+	x = 400,
+	y = 300,
+})
+
